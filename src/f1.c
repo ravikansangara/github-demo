eba5a63593c2f320ebef72cb5d7b0d772112366d
ravikansangara <kansangara.ravi@gmail.com>
2024-08-31 19:55:51 +0530

    Update f1.c and f1.h

--- src/f1.c
+++ src/f1.c
@@ -2,3 +2,8 @@ void f1_first(const char *message)
 {
     printf("f1_first: %s\n", message);
 }
+
+void f1_second(const char *message)
+{
+    printf("f1_second: %s\n", message);
+}
--- src/f1.h
+++ src/f1.h
@@ -1,1 +1,3 @@
 void f1_first(const char *message);
+
+void f1_second(const char *message);
