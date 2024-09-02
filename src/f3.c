e21d0b0481881439b154afe8245a3fdd50036a39
ravikansangara <kansangara.ravi@gmail.com>
2024-09-02 12:41:15 +0530

    Create f3.c and f3.h

---
+++ src/f3.c
@@ -0,0 +1,13 @@
+#include "f3.h"
+
+#include <stdio.h>
+
+void f3_first(const char *message)
+{
+    printf("f3_first: %s\n", message);
+}
+
+void f3_second(const char *message)
+{
+    printf("f3_second: %s\n", message);
+}
