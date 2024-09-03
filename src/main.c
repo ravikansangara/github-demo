4041b12fcb7d8cdb2bc75dd5f9b63e7fc44a8518
Ravi Kansangara <35910522+ravikansangara@users.noreply.github.com>
2024-09-03 07:18:33 +0000

    Fix hotfix of ui crash

--- src/main.c
+++ src/main.c
@@ -5,6 +5,11 @@ void myprintf(const char *message)
     printf("%s", message);
 }
 
+void myprintf_v2(const char *message)
+{
+    printf("%s", message);
+}
+
 int main(int argc, char **argv)
 {
     myprintf("Hello, world!\n");
